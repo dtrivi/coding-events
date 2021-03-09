78330cc7af9e7716390a59bce2a006134dbfb069
dtrivi <danieltrivinos@gmail.com>
2021-03-09 16:14:02 -0600

    11.6.6. Add a Form Handler Method

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,21 +17,28 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    // Make a list accessible to multiple methods, so we create our list at the class level. Otherwise our only list we were working with is inside the displayAllEvents handler, meaning it's only a local variable.
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+        model.addAttribute("events", events); // Recall a static field is one that belongs to the class rather than instances of the class. Means it's shared by every instance of our eventController object.
         return "events/index";
     }
 
-    // lives at /events . Note that @RequestMapping on line 15 establishes an 'events' prefix meaning this is /events/create
+    // lives at /events/create
     @GetMapping("create")
     public String renderCreateEventForm() {
-        return "events/create"; // Have to include 'events/' because 'create.html' is located under the 'events' subfolder in the 'templates' folder.
+        return "events/create";
+    }
+
+    // renderCreateEventForm and createEvent live at the same path, which is OK if they handle different types of requests (GET v. POST)
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) { // We can pull data out of an incoming request using @RequestParam annotation. Name of parameter 'eventName' is the same as our label we used to name our variable in create.html.
+        events.add(eventName);
+        return "redirect:"; // Says return a redirect response that instructs the browser to go to a different page. Typically looks like redirect:/path but in this case we're just going to /events which is set up for the whole class already.
     }
 
 }
