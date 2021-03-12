f634cbe9791277e4e36fa88e67356d4487ecd84e
dtrivi <danieltrivinos@gmail.com>
2021-03-11 19:20:16 -0600

    13.2.3. Applying Validation Annotations

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,32 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
 
-    private int id; // This variable is the start of a unique integer identifier for our class. Each new Event object will have its own unique integer, which will be a way to id each object and tell them apart.
-    private static int nextId = 1; // Static counter. Static means this field belongs to class rather than any one instance of the object. Thus, every single Event object will share the same static 'nextId' field. By initializing this to 1, this is going to be the id of the next object created, and then we can update that counter to 2, so the second object would have the id '2'.
+    private int id;
+    private static int nextId = 1;
 
+    // Adding validation annotations to restrict the range of the values that can be used here.
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
-        this.id = nextId; // Initialize id
-        nextId++; // Then we increment the nextId field to make sure it's unique, which should initialize the id correctly.
+        this.contactEmail = contactEmail;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -31,6 +43,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() { // Don't want to create a setter because we don't want someone to set our id's for us in this case. Also, don't want to expose nextId. It will remain private and only to be used INSIDE the class. Just used for the id initialization.
