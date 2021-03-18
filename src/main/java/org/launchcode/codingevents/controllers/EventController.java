240d5266e029a40708b3991d024abf06d69439d6
dtrivi <danieltrivinos@gmail.com>
2021-03-17 21:15:23 -0500

    18.5.5. Many-to-Many Forms and Data Transfer Objects

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,8 +2,11 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.data.EventRepository;
+import org.launchcode.codingevents.data.TagRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventCategory;
+import org.launchcode.codingevents.models.Tag;
+import org.launchcode.codingevents.models.dto.EventTagDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +29,9 @@ public class EventController {
 
     @Autowired
     private EventCategoryRepository eventCategoryRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
 
     // We want to be able to pass in a query parameter called categoryId, so we add @RequestParam below. The parameter name ('categoryId') needs to match the query parameter name for this to work. Also, we don't want to make the categoryId to be required, so we mark it as false next to the @RequestParam. By making it false, we can just view all events (???).
     @GetMapping
@@ -97,9 +103,45 @@ public class EventController {
             Event event = result.get();
             model.addAttribute("title", event.getName() + " Details");
             model.addAttribute("event", event);
+            model.addAttribute("tags", event.getTags());
         }
 
         return "events/detail";
+    }
+
+    // Add a form that allows users to add a tag to an event. In order to display this form, for a specific event, will have a drop down with all the available tags and allow you to add a tag to it.
+    // responds to /events/add-tag?eventId=13 (13 is just an example)
+    @GetMapping("add-tag")
+    public String displayAddTagForm(@RequestParam Integer eventId, Model model) { // Use model to pass things into the view
+        Optional<Event> result = eventRepository.findById(eventId);
+        Event event = result.get();
+        model.addAttribute("title", "Add Tag to: " + event.getName());
+        model.addAttribute("tags", tagRepository.findAll()); // Passes in a parameter called tags into the template that will contain a list of all of them and we can use that to display a dropdown.
+        // Need to pass in our event object because our form needs to know which event it's attaching a tag to
+        EventTagDTO eventTag = new EventTagDTO();
+        eventTag.setEvent(event);
+        model.addAttribute("eventTag", eventTag); // We're going to use model binding with the data transfer object which will allow us to pass in a single object that can be validated (and that single object will be an instance of EventTagDTO).
+        return "events/add-tag.html";
+    }
+
+    // handler
+    @PostMapping("add-tag")
+    public String processAddTagForm(@ModelAttribute @Valid EventTagDTO eventTag,
+                                    Errors errors,
+                                    Model model) {
+
+        if (!errors.hasErrors()) {
+            Event event = eventTag.getEvent();
+            Tag tag = eventTag.getTag();
+            if (!event.getTags().contains(tag)) {
+                event.addTag(tag);
+                eventRepository.save(event);
+            }
+            return "redirect:detail?eventId=" + event.getId();
+        }
+
+        return "redirect:add-tag";
+
     }
 
 }
